4b0f7771edbb59b06b49f731b9d04f409fa78003
Taylus <Taylus@8e37b1fe-1775-2090-354b-66f21ff69f2f>
2012-06-29 04:55:42 +0000

    Updated world view scrolling logic to work the same way as player movement, replacing "step loop" logic

--- MapDemoGame/MapDemoGame/Player.cs
+++ MapDemoGame/MapDemoGame/Player.cs
@@ -10,9 +10,6 @@ public class Player
     //private const float DIAG_FACTOR = 0.707106781F;
     private const float DIAG_FACTOR = 0.85F;
 
-    //interval by which to scroll the map view, to stop early if a full step would move us beyond map boundaries
-    private const float SCROLL_STEP = 0.1f;
-
     public float Speed { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
@@ -38,7 +35,7 @@ public class Player
     public float ScreenY { get { return ScreenPosition.Y; } }
     public Vector2 ScreenPosition { get { return world.WorldToScreenCoordinates(WorldPosition); } }
 
-    private const float DEFAULT_SPEED = 5.0f;
+    private const float DEFAULT_SPEED = 4.0f;
 
     public Player(World world, Vector2 pos, int w, int h, float speed = DEFAULT_SPEED)
     {
@@ -63,101 +60,64 @@ public class Player
         //      against walls with specified widths, we'll pass through them if our speed > their width (tunneling)
         //      this will be a problem if anything is moving very fast, but I don't think that will be the case anywhere
 
+        //FIX: scrolling the world view in increments makes the player rattle around a bit
+        //     as floating point discrepencies add up between the player's world coords and calculated screen coords
+
         if (keyboard.IsKeyDown(Keys.W))
         {
             //move slower if going diagonally, and move less if we can't move a full step
-            float dist = (keyboard.IsKeyDown(Keys.A) || keyboard.IsKeyDown(Keys.D)) ? Speed * DIAG_FACTOR : Speed;
-            dist = MathHelper.Min(dist, MathHelper.Distance(WorldY, 0));
+            float playerSpeed = (keyboard.IsKeyDown(Keys.A) || keyboard.IsKeyDown(Keys.D)) ? Speed * DIAG_FACTOR : Speed;
+            float playerMoveDist = MathHelper.Min(playerSpeed, MathHelper.Distance(WorldY, 0));
 
             //check if movement is possible (TODO: collisions)
-            WorldY -= dist;
+            WorldY -= playerMoveDist;
 
-            //scroll the map view, if possible
-            if (world.ViewY > 0 && ScreenY < world.ViewHeight / 2)
-            {
-                //scroll in small increments (in case we can't move a full step)
-                for (float i = 0; i < dist; i += SCROLL_STEP)
-                {
-                    world.ViewY -= SCROLL_STEP;
-                    if (world.ViewY < 0)
-                    {
-                        world.ViewY = 0;
-                        break;
-                    }
-                }
-            }
+            //must calculate player and view distances separately, as player may move independently of view
+            float worldWiewScrollDist = MathHelper.Min(playerMoveDist, MathHelper.Distance(world.ViewY, 0));
+            if (ScreenY < world.ViewHeight / 2)
+                world.ViewY -= worldWiewScrollDist;
         }
         else if (keyboard.IsKeyDown(Keys.S))
         {
             //move slower if going diagonally, and move less if we can't move a full step
-            float dist = (keyboard.IsKeyDown(Keys.A) || keyboard.IsKeyDown(Keys.D)) ? Speed * DIAG_FACTOR : Speed;
-            dist = MathHelper.Min(dist, MathHelper.Distance(WorldY + Height, world.HeightPx));
+            float playerSpeed = (keyboard.IsKeyDown(Keys.A) || keyboard.IsKeyDown(Keys.D)) ? Speed * DIAG_FACTOR : Speed;
+            float playerMoveDist = MathHelper.Min(playerSpeed, MathHelper.Distance(WorldY + Height, world.HeightPx));
 
             //check if movement is possible (TODO: collisions)
-            WorldY += dist;
+            WorldY += playerMoveDist;
 
-            //scroll the map view, if possible
-            if ((world.ViewY + world.ViewHeight) < world.HeightPx && ScreenY > world.ViewHeight / 2)
-            {
-                //scroll in small increments (in case we can't move a full step)
-                for (float i = 0; i < dist; i += SCROLL_STEP)
-                {
-                    world.ViewY += SCROLL_STEP;
-                    if (world.ViewY + world.ViewHeight > world.HeightPx)
-                    {
-                        world.ViewY = world.HeightPx - world.ViewHeight;
-                        break;
-                    }
-                }
-            }
+            //must calculate player and view distances separately, as player may move independently of view
+            float worldWiewScrollDist = MathHelper.Min(playerMoveDist, MathHelper.Distance(world.ViewY + world.ViewHeight, world.HeightPx));
+            if (ScreenY >= world.ViewHeight / 2)
+                world.ViewY += worldWiewScrollDist;
         }
         if (keyboard.IsKeyDown(Keys.A))
         {
             //move slower if going diagonally, and move less if we can't move a full step
-            float dist = (keyboard.IsKeyDown(Keys.W) || keyboard.IsKeyDown(Keys.S)) ? Speed * DIAG_FACTOR : Speed;
-            dist = MathHelper.Min(dist, MathHelper.Distance(WorldX, 0));
+            float playerSpeed = (keyboard.IsKeyDown(Keys.W) || keyboard.IsKeyDown(Keys.S)) ? Speed * DIAG_FACTOR : Speed;
+            float playerMoveDist = MathHelper.Min(playerSpeed, MathHelper.Distance(WorldX, 0));
 
             //check if movement is possible (TODO: collisions)
-            WorldX -= dist;
+            WorldX -= playerMoveDist;
 
-            //scroll the map view, if possible
-            if (world.ViewX > 0 && ScreenX < world.ViewWidth / 2)
-            {
-                //scroll in small increments (in case we can't move a full step)
-                for (float i = 0; i < dist; i += SCROLL_STEP)
-                {
-                    world.ViewX -= SCROLL_STEP;
-                    if (world.ViewX < 0)
-                    {
-                        world.ViewX = 0;
-                        break;
-                    }
-                }
-            }
+            //must calculate player and view distances separately, as player may move independently of view
+            float worldWiewScrollDist = MathHelper.Min(playerMoveDist, MathHelper.Distance(world.ViewX, 0));
+            if (ScreenX < world.ViewWidth / 2)
+                world.ViewX -= worldWiewScrollDist;
         }
         else if (keyboard.IsKeyDown(Keys.D))
         {
             //move slower if going diagonally, and move less if we can't move a full step
-            float dist = (keyboard.IsKeyDown(Keys.W) || keyboard.IsKeyDown(Keys.S)) ? Speed * DIAG_FACTOR : Speed;
-            dist = MathHelper.Min(dist, MathHelper.Distance(WorldX + Width, world.WidthPx));
+            float playerSpeed = (keyboard.IsKeyDown(Keys.W) || keyboard.IsKeyDown(Keys.S)) ? Speed * DIAG_FACTOR : Speed;
+            float playerMoveDist = MathHelper.Min(playerSpeed, MathHelper.Distance(WorldX + Width, world.WidthPx));
 
             //check if movement is possible (TODO: collisions)
-            WorldX += dist;
+            WorldX += playerMoveDist;
 
-            //scroll the map view, if possible
-            if ((world.ViewX + world.ViewWidth) < world.WidthPx && ScreenX > world.ViewWidth / 2)
-            {
-                //scroll in small increments (in case we can't move a full step)
-                for (float i = 0; i < dist; i += SCROLL_STEP)
-                {
-                    world.ViewX += SCROLL_STEP;
-                    if (world.ViewX + world.ViewWidth > world.WidthPx)
-                    {
-                        world.ViewX = world.WidthPx - world.ViewWidth;
-                        break;
-                    }
-                }
-            }
+            //must calculate player and view distances separately, as player may move independently of view
+            float worldWiewScrollDist = MathHelper.Min(playerMoveDist, MathHelper.Distance(world.ViewX + world.ViewWidth, world.WidthPx));
+            if (ScreenX >= world.ViewWidth / 2)
+                world.ViewX += worldWiewScrollDist;
         }
     }
 }
